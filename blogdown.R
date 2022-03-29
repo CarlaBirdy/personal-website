fb8f4787dc308e92e6a77ca27ff7c32022e7c6a4
Carla Archibald <c.archibald@deakin.edu.au>
2022-03-29 14:24:48 +1100

    Configure R blogger

---
+++ blogdown.R
@@ -0,0 +1,14 @@
+####################################################################################
+#
+# Create personal website using blogdown
+#    By: Dr Carla Archibald
+#    Created on: 29/03/2022
+#     
+####################################################################################
+
+# wd, data & packages #####
+install.packages("blogdown") # install the blogdown package
+library(blogdown) # load blogdown
+
+# start new site
+new_site(theme = "wowchemy/starter-academic") # create your website!
